cd625804d938580c0ae464df5e95b3f499a5d959
Shift <shift@laravelshift.com>
2024-06-12 10:33:15 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\NovaServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\JetstreamServiceProvider::class,
 ];
