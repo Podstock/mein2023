6bbd8178df51edc0b2fe4812b1dd3ff938381daa
Sebastian Reimers <hallo@studio-link.de>
2024-10-09 16:30:30 +0200

    fix mastodon user

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -13,10 +13,16 @@ class User extends JsonResource
      */
     public function toArray($request): array
     {
+        $m_url = str_replace('http://', '', $this->mastodon);
+        $m_url = str_replace('https://', '', $m_url);
+        $m = parse_url("https://".$m_url);
+        $m_user = str_replace('@', '', $m['user']);
+        $mastodon = "https://".$m['host']."/@".$m_user;
+
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'mastodon' => $this->mastodon,
+            'mastodon' => $mastodon,
             'sendegate' => str_replace('@', '', $this->sendegate),
             'avatar' => $this->avatar,
             'search' => $this->search,
