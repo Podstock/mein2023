a04879328c8749ddd2899bfa3b82177c9080cab7
Sebastian Reimers <hallo@studio-link.de>
2024-10-09 21:23:15 +0200

    re-enable pretalx

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -57,6 +57,9 @@ const logout = () => {
                                 <NavLink :href="route('users')" :active="route().current('users')">
                                   Teilnehmer*innen
                                 </NavLink>
+                                <NavLink :href="route('fahrplan')" :active="route().current('fahrplan')">
+                                  Fahrplan
+                                </NavLink>
                             </div>
                         </div>
 
@@ -134,6 +137,9 @@ const logout = () => {
                         </ResponsiveNavLink>
                         <ResponsiveNavLink :href="route('users')" :active="route().current('users')">
                             Teilnehmer*innen
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('fahrplan')" :active="route().current('fahrplan')">
+                            Fahrplan
                         </ResponsiveNavLink>
                     </div>
 
